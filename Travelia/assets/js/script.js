59c470b6a56a76cbeefb20af5e391cf4b2ffd9f1
Prithviraj More <143012269+Prithxvhie44@users.noreply.github.com>
2025-01-04 20:28:15 +0530

    new

--- Travelia/assets/js/script.js
+++ Travelia/assets/js/script.js
@@ -3,3 +3,21 @@ const preloader = document.querySelector('[data-preloader]');
 window.addEventListener('load', () => { 
     preloader.classList.add('preloader-remove');
 });
+
+
+const addEventOnlements = function(elements, eventTypes, callback) {
+    for(let i=0, len=elements.length; i<len; i++) {
+        elements[i].addEventListener(eventTypes, callback);
+        }
+    }
+
+const navbar = document.querySelector('[data-navbar]');
+const navbarTogglers = document.querySelector('[data-nav-toggler]');
+const overlay = document.querySelector('[data-overlay]');
+
+const toggleNav = function() {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("nav-active");}
+
+addEventOnlements(navbarTogglers, 'click', toggleNav);
