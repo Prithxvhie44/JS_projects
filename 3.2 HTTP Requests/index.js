85fe30ebe326ed69dca93e50a70b1067f58aef80
Prithviraj More <143012269+Prithxvhie44@users.noreply.github.com>
2025-01-19 19:42:30 +0530

    Index js completed

--- 3.2 HTTP Requests/index.js
+++ 3.2 HTTP Requests/index.js
@@ -1,10 +1,21 @@
 import express from 'express';
 
 const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 }); 
+
+app.get('/contact', (req, res) => {
+    res.send('Hello World!');
+  }); 
+  
+
+app.get('/about', (req, res) => {
+    res.send(<h1>About me section</h1>);
+  }); 
+  
 
 app.listen(port, () => {    
   console.log('Server is running on',port);
